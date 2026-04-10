7b2b4d353a5fa6d683f3cc9d1ee02e69633a1b7f
bemapa3 <db.studio.ai@gmail.com>
2026-04-10 18:06:27 +0700

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,7 +1,10 @@
-try:
-    from .flux_auto_tile_pro import NODE_CLASS_MAPPINGS, NODE_DISPLAY_NAME_MAPPINGS
-except ImportError:
-    # Trường hợp file tên là flux_tile_blend.py thì dùng dòng dưới
-    from .flux_tile_blend import NODE_CLASS_MAPPINGS, NODE_DISPLAY_NAME_MAPPINGS
+import os
+import sys
+
+# Đảm bảo Python tìm thấy thư mục hiện tại
+sys.path.append(os.path.dirname(__file__))
+
+# Gọi các Node từ file flux_tile_pro_v1
+from flux_tile_pro_v1 import NODE_CLASS_MAPPINGS, NODE_DISPLAY_NAME_MAPPINGS
 
 __all__ = ['NODE_CLASS_MAPPINGS', 'NODE_DISPLAY_NAME_MAPPINGS']
